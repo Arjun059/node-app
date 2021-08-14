8d0ac2d0a08310aef1c3872897af8e557c51e5f8
Arjun059 <80092032+Arjun059@users.noreply.github.com>
2021-08-14 15:00:42 +0530

    work on ui

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require("express");
+const path = require('path');
+
+require("dotenv").config();
+
+//------express init---------
 const app = express();
-let PORT = process.env.PORT || 8200 ;
+
+//------static folder-----------
+app.use(express.static("public"));
+
+//-----body parseer------------
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//------view engine setup------
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname + "/views"));
+
+//-------Routes----------------
 
 app.get('/', (req, res) => {
-    res.send("Hello, How are you!")
+    res.render("index")
 });
+
+// ---server running-----------
+let PORT = process.env.PORT || 8200 ;
 app.listen(PORT, ()=>console.log(`http://localhost:${PORT}/`));
